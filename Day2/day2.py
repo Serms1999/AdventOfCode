c1f1d401631a2260ed5a4d4fb05f17408849a31b
Serms1999 <serms1999@gmail.com>
2022-07-19 20:09:12 +0200

    Day 2 complete

---
+++ Day2/day2.py
@@ -0,0 +1,49 @@
+import IO.IO_module as IO
+
+
+def part1(movements: list) -> int:
+    position = 0
+    depth = 0
+
+    for move in movements:
+        s = move.split()
+        direction, value = s[0], int(s[1])
+
+        if direction == 'forward':
+            position += value
+        elif direction == 'down':
+            depth += value
+        elif direction == 'up':
+            depth -= value
+
+    return position * depth
+
+
+def part2(movements: list) -> int:
+    aim = 0
+    position = 0
+    depth = 0
+
+    for move in movements:
+        s = move.split()
+        direction, value = s[0], int(s[1])
+
+        if direction == 'forward':
+            position += value
+            depth += value * aim
+        elif direction == 'down':
+            aim += value
+        elif direction == 'up':
+            aim -= value
+
+    return position * depth
+
+
+def main():
+    input_lines = IO.read_input_lines()
+    print(f'Part one solution: {part1(input_lines)}')
+    print(f'Part two solution: {part2(input_lines)}')
+
+
+if __name__ == '__main__':
+    main()
