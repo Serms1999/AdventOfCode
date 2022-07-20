2b92f72d4f2d9bb7ee34dadcaacf58c964218deb
Serms1999 <serms1999@gmail.com>
2022-07-20 12:33:53 +0200

    Allow to change file input name

--- IO/IO_module.py
+++ IO/IO_module.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 
-def read_input_lines():
-    current_input = os.path.join(os.getcwd(), 'input')
+def read_input_lines(file_name='input'):
+    current_input = os.path.join(os.getcwd(), file_name)
     try:
         with open(current_input, 'r') as input_file:
             lines = input_file.read().splitlines()
