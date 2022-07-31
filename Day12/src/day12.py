1297959732227ba0dcb00979789d88344e6bd4db
Serms1999 <serms1999@gmail.com>
2022-07-31 22:19:38 +0200

    Working on day 12

---
+++ Day12/src/day12.py
@@ -0,0 +1,19 @@
+from IO.IO_module import read_input_lines
+
+
+def part1(input_lines: list) -> int:
+    return 0
+
+
+def part2(input_lines: list) -> int:
+    return 0
+
+
+def main():
+    input_lines = read_input_lines(root_file=__file__)
+    print(f'Part one solution: {part1(input_lines)}')
+    print(f'Part two solution: {part2(input_lines)}')
+
+
+if __name__ == '__main__':
+    main()
