2951ed7e3a5da3c2c6160f55896414077f63f556
Serms1999 <serms1999@gmail.com>
2022-10-13 17:45:39 +0200

    Working on day 15

--- Day15/src/day15.py
+++ Day15/src/day15.py
@@ -51,7 +51,7 @@ def part2(input_lines: list) -> int:
 def main():
     input_lines = read_input_lines(root_file=__file__)
     print(f'Part one solution: {part1(input_lines)}')
-    print(f'Part two solution: {part2(input_lines)}')
+    print(f'Part two solution: {part1(input_lines)}')
 
 
 if __name__ == '__main__':
