18ca941cfb92e74ff79e548dfc49da81b55bcb62
Sergio Marin Sanchez <serms1999@gmail.com>
2023-12-14 02:46:37 +0100

    Day 2 Part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -30,9 +30,46 @@ def check_games(lines: list[str]) -> int:
     return total
 
 
+def get_set_required_cubes(set: str) -> dict[str, int]:
+    required_cubes = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+
+    for bag in set.strip().replace(', ', ',').split(','):
+        num, color = bag.split(' ')
+        required_cubes[color] = int(num)
+
+    return required_cubes
+
+
+def get_games_power(lines: list[str]) -> int:
+    total = 0
+    for index, line in enumerate(lines, start=1):
+        _, sets = line.split(':')
+        sets = sets[1:].split(';')
+
+        required_cubes = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        }
+
+        for set_requirements in [get_set_required_cubes(set) for set in sets]:
+            required_cubes['red'] = max(required_cubes['red'], set_requirements['red'])
+            required_cubes['green'] = max(required_cubes['green'], set_requirements['green'])
+            required_cubes['blue'] = max(required_cubes['blue'], set_requirements['blue'])
+
+        total += required_cubes['red'] * required_cubes['green'] * required_cubes['blue']
+
+    return total
+
+
 def main():
     input_lines = read_input_lines(file_name='input')
     print(f'Sum of the valid games: {check_games(input_lines)}')
+    print(f'Sum of the powers: {get_games_power(input_lines)}')
 
 
 if __name__ == '__main__':
