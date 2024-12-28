7c8160b06ded680760b1b211702ad28933444274
Sergio Marin Sanchez <serms1999@gmail.com>
2024-12-29 00:51:46 +0100

    Add[28/12/2024] Day 16 Year 2024

---
+++ 2024/Day16/Day16.py
@@ -0,0 +1,100 @@
+from IO import read_input_lines, format_output
+from unittest import TestCase, TestResult, TestLoader, TextTestRunner
+from math import inf
+from heapq import heappop, heappush
+from collections.abc import Generator
+
+
+class PartialTests(TestCase):
+    def test_1_part1(self):
+        self.assertEqual(maze_score(read_input_lines(file_name='test_input_1')), 7036)
+
+    def test_2_part1(self):
+        self.assertEqual(maze_score(read_input_lines(file_name='test_input_2')), 11048)
+
+    def test_1_part2(self):
+        self.assertEqual(maze_path(read_input_lines(file_name='test_input_1')), 45)
+
+    def test_2_part2(self):
+        self.assertEqual(maze_path(read_input_lines(file_name='test_input_2')), 64)
+
+
+def get_neighbors(cell: tuple[int, int], direction: int) -> Generator[tuple[int, tuple[int, int], int]]:
+    directions: list[tuple[int, int]] = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    yield 1000, cell, (direction - 1) % len(directions)
+    yield 1000, cell, (direction + 1) % len(directions)
+    new_x, new_y = cell[0] + directions[direction][0], cell[1] + directions[direction][1]
+    yield 1, (new_x, new_y), direction
+
+
+def dijkstra(input_lines: list[str]) -> tuple[int, set[tuple[int, int]]]:
+    distances: dict[tuple[tuple[int, int], int], int | float] = {}
+    previous_cells: dict[tuple[tuple[int, int], int], set[tuple[tuple[int, int], int]]] = {}
+    start: tuple[int, int] = (0, 0)
+    end: tuple[int, int] = (0, 0)
+    for i in range(len(input_lines)):
+        for j in range(len(input_lines[i])):
+            if input_lines[i][j] != '#':
+                if input_lines[i][j] == 'S':
+                    start = (i, j)
+                elif input_lines[i][j] == 'E':
+                    end = (i, j)
+
+    queue: list[tuple[int, tuple[int, int], int]] = []
+    heappush(queue, (0, start, 0))
+    best_cost: int | float = inf
+    end_states: set[tuple[int, int], int] = set()
+
+    while queue:
+        current_distance, current_cell, current_direction = heappop(queue)
+        if current_distance > distances.get((current_cell, current_direction), inf):
+            continue
+        distances[(current_cell, current_direction)] = current_distance
+        if current_cell == end:
+            if current_distance > best_cost:
+                continue
+            best_cost = current_distance
+            end_states.add((current_cell, current_direction))
+        for neighbor_distance, neighbor_cell, neighbor_direction in get_neighbors(current_cell, current_direction):
+            if input_lines[neighbor_cell[0]][neighbor_cell[1]] == '#':
+                continue
+            if current_distance + neighbor_distance > distances.get((neighbor_cell, neighbor_direction), inf):
+                continue
+            if current_distance + neighbor_distance < distances.get((neighbor_cell, neighbor_direction), inf):
+                distances[(neighbor_cell, neighbor_direction)] = current_distance + neighbor_distance
+                previous_cells[(neighbor_cell, neighbor_direction)] = {(current_cell, current_direction)}
+            elif current_distance + neighbor_distance == distances.get((neighbor_cell, neighbor_direction), inf):
+                previous_cells[(neighbor_cell, neighbor_direction)].add((current_cell, current_direction))
+            heappush(queue, (current_distance + neighbor_distance, neighbor_cell, neighbor_direction))
+
+    stack: list[tuple[tuple[int, int], int]] = list(end_states)
+    cells: set[tuple[int, int]] = {end}
+    while stack:
+        cell = stack.pop(-1)
+        for previous_cell in previous_cells.get(cell, []):
+            if previous_cell not in cells:
+                stack.append(previous_cell)
+                cells.add(previous_cell[0])
+
+    return best_cost, cells
+
+def maze_score(input_lines: list[str]) -> int:
+    return dijkstra(input_lines)[0]
+
+
+def maze_path(input_lines: list[str]) -> int:
+    _, visited_nodes = dijkstra(input_lines)
+    return len(visited_nodes)
+
+def main() -> None:
+    result: TestResult = TextTestRunner().run(TestLoader().loadTestsFromTestCase(PartialTests))
+    if result.wasSuccessful():
+        input_lines: list[str] = read_input_lines(file_name='input')
+        format_output(
+            output_part1=f'Maze score: {maze_score(input_lines)}',
+            output_part2=f'Maze path: {maze_path(input_lines)}'
+        )
+
+
+if __name__ == '__main__':
+    main()
--- 2024/README.md
+++ 2024/README.md
@@ -29,7 +29,7 @@ Collect stars by solving puzzles. Two puzzles will be made available on each day
 | 13  | :white_check_mark:      | :white_check_mark:      | [Day 13](Day13) | [Day 13](https://adventofcode.com/2024/day/13) |
 | 14  | :white_check_mark:      | :white_check_mark:      | [Day 14](Day14) | [Day 14](https://adventofcode.com/2024/day/14) |
 | 15  | :white_check_mark:      | :white_check_mark:      | [Day 15](Day15) | [Day 15](https://adventofcode.com/2024/day/15) |
-| 16  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 16](https://adventofcode.com/2024/day/16) |
+| 16  | :white_check_mark:      | :white_check_mark:      | [Day 16](Day16) | [Day 16](https://adventofcode.com/2024/day/16) |
 | 17  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 17](https://adventofcode.com/2024/day/17) |
 | 18  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 18](https://adventofcode.com/2024/day/18) |
 | 19  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 19](https://adventofcode.com/2024/day/19) |
