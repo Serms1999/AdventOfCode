55496cf8d56632c75c16544a6856ca764dcea3ff
Sergio Marin Sanchez <serms1999@gmail.com>
2025-10-19 17:39:37 +0200

    [19/10/2025] Update project to use uv

--- IO/IO_module.py
+++ IO/IO_module.py
@@ -1,22 +1,21 @@
-import os
-import sys
+from pathlib import Path
+from sys import stderr, exit
 import inspect
 
-from typing import List, Optional
 
-
-def read_input_lines(file_name: str ='input') -> List[str]:
+def read_input_lines(file_name: str ='input') -> list[str]:
     caller_file = inspect.getouterframes(inspect.currentframe(), 2)[1][1]
-    current_path = os.path.dirname(caller_file)
-    file_path = os.path.join(current_path, file_name)
+    current_path: Path = Path(caller_file).parent
+    file_path: Path = current_path / file_name
     try:
         with open(file_path, 'r') as input_file:
             return input_file.read().splitlines()
     except FileNotFoundError:
-        print(f'No such file or directory: \'{file_path}\'', file=sys.stderr)
-        sys.exit(1)
+        print(f'No such file or directory: \'{file_path}\'', file=stderr)
+        exit(1)
+    return []
 
-def format_output(output_part1: str, output_part2: Optional[str] = None) -> None:
+def format_output(output_part1: str, output_part2: str | None = None) -> None:
     fill_number: int = max(len(output_part1), len(output_part2)) if output_part2 is not None else len(output_part1)
     fill_number = (fill_number - len(' Part X ') + 1) // 2
     print(f'{"-" * fill_number} Part 1 {"-" * fill_number}')
