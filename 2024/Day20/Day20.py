d5ad4e55417673ad78d0876e1ec2d2a0b97f3441
Sergio Marin Sanchez <serms1999@gmail.com>
2025-11-10 02:39:12 +0100

    [10/11/2025] Day 20 Year 2024

---
+++ 2024/Day20/Day20.py
@@ -0,0 +1,104 @@
+from IO import read_input_lines, format_output
+from unittest import TestCase, TestResult, TestLoader, TextTestRunner
+from collections import deque
+from collections.abc import Generator
+from itertools import permutations
+from math import inf
+
+
+class PartialTests(TestCase):
+    def test_part1(self):
+        self.assertEqual(count_savings(read_input_lines(file_name='test_input'), max_cheat=2, min_improvement=0), 44)
+    def test_part2(self):
+        self.assertEqual(count_savings(read_input_lines(file_name='test_input'), max_cheat=20, min_improvement=50), 285)
+
+
+def get_adjacent_nodes(racetrack: set[tuple[int, int]], node: tuple[int, int]) -> Generator[tuple[int, int], None, None]:
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if abs(i) == abs(j): continue
+            if (node[0] + i, node[1] + j) not in racetrack: continue
+            yield node[0] + i, node[1] + j
+
+
+
+def bfs(racetrack: set[tuple[int, int]], start: tuple[int, int], end: tuple[int, int]) -> dict[tuple[int, int], int]:
+    queue: deque[tuple[int, int]] = deque()
+    queue.append(start)
+    distances: dict[tuple[int, int], int] = {start: 0}
+    while queue:
+        current_node: tuple[int, int] = queue.popleft()
+        if current_node == end: break
+        for neighbor in get_adjacent_nodes(racetrack, current_node):
+            if neighbor not in distances:
+                distances[neighbor] = distances.get(current_node, 0) + 1
+                queue.append(neighbor)
+
+    return distances
+
+
+def at_least_n_picoseconds(savings: dict[int, int], n: int = 100) -> int:
+    return sum(count for saving, count in savings.items() if saving >= n)
+
+
+def manhattan_distance(node1: tuple[int, int], node2: tuple[int, int]) -> int:
+    return abs(node1[0] - node2[0]) + abs(node1[1] - node2[1])
+
+
+def get_racetrack(input_lines: list[str]) -> tuple[set[tuple[int, int]], tuple[int, int], tuple[int, int]]:
+    start: tuple[int, int] = (0, 0)
+    end: tuple[int, int] = (0, 0)
+    nodes: set[tuple[int, int]] = set()
+    for row, line in enumerate(input_lines):
+        for col, cell in enumerate(line):
+            if cell == '#': continue
+            nodes.add((row, col))
+            if cell == 'S':
+                start: tuple[int, int] = (row, col)
+            elif cell == 'E':
+                end: tuple[int, int] = (row, col)
+
+    return nodes, start, end
+
+
+def get_savings(racetrack: set[tuple[int, int]], start:tuple[int, int], end: tuple[int, int], max_cheat: int) -> dict[int, int]:
+    distances_from_start: dict[tuple[int, int], int] = bfs(racetrack, start=start, end=end)
+    distances_from_end: dict[tuple[int, int], int] = bfs(racetrack, start=end, end=start)
+
+    original_distance: int = distances_from_start.get(end, inf)
+
+    savings: dict[int, int] = {}
+    for node1, node2 in permutations(racetrack, r=2):
+        if (dist := manhattan_distance(node1, node2)) <= max_cheat:
+            new_distance: int = distances_from_start[node1] + dist + distances_from_end[node2]
+            saving: int = original_distance - new_distance
+            if saving > 0:
+                savings[saving] = savings.get(saving, 0) + 1
+
+    return savings
+
+
+def count_savings(input_lines: list[str], max_cheat: int, min_improvement: int = 100) -> int:
+    racetrack: set[tuple[int, int]]
+    start: tuple[int, int]
+    end: tuple[int, int]
+    racetrack, start, end = get_racetrack(input_lines)
+
+    savings: dict[int, int] = get_savings(racetrack, start, end, max_cheat)
+
+    return at_least_n_picoseconds(savings, n=min_improvement)
+
+
+def main() -> None:
+    result: TestResult = TextTestRunner().run(TestLoader().loadTestsFromTestCase(PartialTests))
+    if result.wasSuccessful():
+        input_lines: list[str] = read_input_lines(file_name='input')
+        format_output(
+            output_part1=f'Steps to exit: {count_savings(input_lines, max_cheat=2, min_improvement=100)}',
+            output_part2=f'Steps to exit: {count_savings(input_lines, max_cheat=20, min_improvement=100)}',
+        )
+
+
+
+if __name__ == '__main__':
+    main()
--- 2024/README.md
+++ 2024/README.md
@@ -33,7 +33,7 @@ Collect stars by solving puzzles. Two puzzles will be made available on each day
 | 17  | :white_check_mark:      | :white_check_mark:      | [Day 17](Day17) | [Day 17](https://adventofcode.com/2024/day/17) |
 | 18  | :white_check_mark:      | :white_check_mark:      | [Day 18](Day18) | [Day 18](https://adventofcode.com/2024/day/18) |
 | 19  | :white_check_mark:      | :white_check_mark:      | [Day 19](Day19) | [Day 19](https://adventofcode.com/2024/day/19) |
-| 20  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 20](https://adventofcode.com/2024/day/20) |
+| 20  | :white_check_mark:      | :white_check_mark:      | [Day 20](Day20) | [Day 20](https://adventofcode.com/2024/day/20) |
 | 21  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 21](https://adventofcode.com/2024/day/21) |
 | 22  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 22](https://adventofcode.com/2024/day/22) |
 | 23  | :ballot_box_with_check: | :ballot_box_with_check: |                 | [Day 23](https://adventofcode.com/2024/day/23) |
